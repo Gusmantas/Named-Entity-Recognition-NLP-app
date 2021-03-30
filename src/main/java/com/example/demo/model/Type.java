c5ae1cff646392a0c26fe510f6681d4d12f8731f
Mantas <gusmantas@gmail.com>
2021-03-30 16:13:25 +0200

    init commit

---
+++ src/main/java/com/example/demo/model/Type.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+public enum Type {
+    PERSON("Person"),
+    CITY("City"),
+    STATE_OR_PROVINCE("State_Or_Province"),
+    COUNTRY("Country"),
+    EMAIL("Email"),
+    TITLE("Title");
+
+    private String type;
+    Type(String type){
+        this.type = type;
+    }
+
+    public String getName(){
+        return type;
+    }
+ }
